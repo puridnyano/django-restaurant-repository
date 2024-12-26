f493ea9848b17bf15a6a9cb75300ab2670789892
Dnyaneshwari Puri <puri.dnyano@gmail.com>
2024-12-26 06:56:36 +0530

    Initial commit

---
+++ Base_App/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def HomeView(request):
+    pass
+
+def AboutView(request):
+    pass
+
+def MenuView(request):
+    pass
+
+def BookTableView(request):
+    pass
