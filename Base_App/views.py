efccd5eb8c4683dadbfb38be19cfc6cf507beedc
Dnyaneshwari Puri <puri.dnyano@gmail.com>
2025-01-10 08:10:12 +0530

    Data Added in database

--- Base_App/views.py
+++ Base_App/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Base_App.models import Item, ItemList, AboutUs, Feedback, BookTable
 # Create your views here.
 
 def HomeView(request):
-    return render(request,'home.html')
+    items = Item.objects.all()
+    list = ItemList.objects.all()
+    return render(request,'home.html', {'items':items, 'list':list})
 
 def AboutView(request):
     return render(request,'about.html')
 
 def MenuView(request):
-    return render(request,'menu.html')
+    items = Item.objects.all()
+    list = ItemList.objects.all()
+    return render(request,'menu.html', {'items':items, 'list':list})
 
 def BookTableView(request):
     return render(request,'book_table.html')
